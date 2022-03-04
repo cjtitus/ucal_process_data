fdbc0e7b08dd185f65340ebad1cd168dc640fa8e
Charles Titus <ctitus@bnl.gov>
2022-03-04 14:59:59 -0500

    catalog tools written, linting

--- ucalpost/catalog_utils.py
+++ ucalpost/catalog_utils.py
@@ -1,4 +1,5 @@
-from databroker.queries import TimeRange, FullText
+from databroker.queries import TimeRange, FullText, PartialUID, RawMongo
+
 
 def filter_by_stop(catalog):
     ok_uuids = []
@@ -10,4 +11,36 @@ def filter_by_stop(catalog):
         except:
             pass
     return catalog.search(PartialUID(*ok_uuids))
- 
+
+
+def filter_by_sample(catalog, samplename):
+    return catalog.search({"sample_args.sample_name.value": samplename})
+
+
+def filter_by_group(catalog, groupname):
+    return catalog.search({"group": groupname})
+
+
+def list_groups(catalog):
+    return list_start_key_vals(catalog, "group")
+
+
+def list_samples(catalog):
+    return list_start_key_vals(catalog, "sample_args", "sample_name", "value")
+
+
+def list_edges(catalog):
+    return list_start_key_vals(catalog, "edge")
+
+
+def list_start_key_vals(catalog, *keys):
+    vals = set()
+    for h in catalog.values():
+        s = h.metadata['start']
+        for k in keys:
+            s = s.get(k, None)
+            if s is None:
+                break
+        if s is not None:
+            vals.add(s)
+    return vals
