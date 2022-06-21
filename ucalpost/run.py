f23351f6ae96ec1438dea37d25ce5bdbfaf02312
Charles Titus <ctitus@bnl.gov>
2022-06-21 15:06:23 -0400

    run summary and file-finding updated

--- ucalpost/run.py
+++ ucalpost/run.py
@@ -10,8 +10,13 @@ def get_config_dict(run):
     return run.primary.descriptors[0]['configuration']['tes']['data']
 
 
-def get_filename(run):
+def get_filename(run, convert_local=True):
     filename = get_config_dict(run)['tes_filename']
+    if convert_local:
+        raw_directory = get_raw_directory(run)
+        a, name = path.split(filename)
+        _, runNumber = path.split(a)
+        filename = path.join(raw_directory, runNumber, name)
     return filename
 
 
@@ -25,7 +30,14 @@ def get_logname(run):
 
 
 def get_save_directory(run):
-    basename = "/nsls2/data/sst1/legacy/ucal/processed"
+    basename = "/nsls2/data/sst/legacy/ucal/processed"
+    timestamp = run.metadata['start']['time']
+    date = datetime.datetime.fromtimestamp(timestamp)
+    return path.join(basename, f"{date.year}/{date.month:02d}/{date.day:02d}")
+
+
+def get_raw_directory(run):
+    basename = "/nsls2/data/sst/legacy/ucal/raw"
     timestamp = run.metadata['start']['time']
     date = datetime.datetime.fromtimestamp(timestamp)
     return path.join(basename, f"{date.year}/{date.month:02d}/{date.day:02d}")
@@ -47,7 +59,7 @@ def get_line_names(cal_run):
 
 
 def get_cal(run):
-    return db['ec72ac48']
+    return db[run.start['last_cal']]
 
 
 def getRunFromStop(doc):
@@ -57,22 +69,28 @@ def getRunFromStop(doc):
 
 
 def get_samplename(run):
-    return run.start['sample_args']['sample_name']['value']
+    if 'sample_args' in run.start:
+        return run.start['sample_args']['sample_name']['value']
+    else:
+        return "None"
 
 
 def summarize_run(run):
     scanid = run.metadata['start']['scan_id']
     sample = get_samplename(run)
-    scantype = run.metadata['start']['scantype']
-    if scantype == 'xas':
-        edge = run.metadata['start']['edge']
+    scantype = run.start.get('scantype', 'None')
+
     print(f"Scan {scanid}")
+    if 'group' in run.start:
+        print(f"Group: {run.start['group']}")
     print(f"Sample name: {sample}")
     if scantype == 'xas':
-        edge = run.metadata['start']['edge']
+        edge = run.start.get('edge', 'Not recorded')
         print(f"Scantype: {scantype}, edge: {edge}")
     else:
         print(f"Scantype: {scantype}")
+    if 'last_cal' in run.start:
+        print(f"Calibration: {run.start['last_cal'][:8]}")
 
 #tes_runs = db.search(TimeRange(since="2022-01-26", until="2022-01-28"))
 #sample_runs = tes_runs.search({"sample_args": {"$exists": True}})
