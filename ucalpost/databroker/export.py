e157e8bee6de4b1c24645456aee21f4a7d3e7738
Charles Titus <ctitus@bnl.gov>
2024-06-03 23:30:24 -0400

    updates to export for NEXAFS move

--- ucalpost/databroker/export.py
+++ ucalpost/databroker/export.py
@@ -1,25 +1,40 @@
 from ..tes.process_classes import scandata_from_run
 from xastools.utils import roiMaster, roiDefaults
-from xastools.io import exportXASToYaml
-from xastools.xas import XAS
-from .run import get_proposal_directory
-import os
+from .run import get_samplename, get_sampleid
 from tiled.queries import Key
-from os import path
 import datetime
 import numpy as np
-from functools import reduce
 from httpx import HTTPStatusError
 from time import sleep
 import re
+
 """
 Module to export processed data to analysis catalog
 """
 
 ANALYSIS_CATALOG = None
 
+
 def convert_names(name):
-    name_conversions = {"en_energy_setpoint": "MONO", "en_energy": "ENERGY_ENC", "ucal_I400_i0up": "I0", "ucal_I400_ref": "REF", "ucal_I400_sc": "SC", "ucal_i400_i0up": "I0", "ucal_i400_ref": "REF", "ucal_i400_sc": "SC", "tes_tfy": "tfy", 'ucal_i0up': 'I0', 'ucal_ref': 'REF', 'ucal_sc': 'SC'}
+    name_conversions = {
+        "en_energy_setpoint": "MONO",
+        "en_energy": "ENERGY_ENC",
+        "ucal_I400_i0up": "I0",
+        "ucal_I400_ref": "REF",
+        "ucal_I400_sc": "SC",
+        "ucal_i400_i0up": "I0",
+        "ucal_i400_ref": "REF",
+        "ucal_i400_sc": "SC",
+        "tes_tfy": "tfy",
+        "ucal_i0up": "I0",
+        "nexafs_i0up": "I0",
+        "ucal_ref": "REF",
+        "ucal_sc": "SC_UCAL",
+        "nexafs_sc": "SC",
+        "nexafs_pey": "PEY",
+        "nexafs_ref": "REF",
+        "nexafs_i1": "I1",
+    }
     return name_conversions.get(name, name)
 
 
@@ -42,54 +57,91 @@ def get_with_fallbacks(thing, *possible_names, default=None):
 def get_run_header(run):
     metadata = {}
     scaninfo = {}
-    scaninfo['sample'] = run.start['sample_md']['name']
-    scaninfo['loadid'] = run.start['sample_md']['sample_id']
-    scaninfo['scan'] = run.start['scan_id']
-    scaninfo['date'] = datetime.datetime.fromtimestamp(run.start['time']).isoformat()
-    scaninfo['command'] = get_with_fallbacks(run.start, 'command', 'plan_name', default=None)
-    scaninfo['element'] = get_with_fallbacks(run.start, 'element', 'edge', default=None)
-    scaninfo['motor'] = convert_names(run.start['motors'][0])
-    scankeys = ['time', 'users', 'proposal', 'cycle', 'saf', 'group_md', 'beamtime_uid', 'beamtime_start', 'repeat']
+    scaninfo["sample"] = get_samplename(run)
+    scaninfo["loadid"] = get_sampleid(run)
+    scaninfo["scan"] = run.start["scan_id"]
+    scaninfo["date"] = datetime.datetime.fromtimestamp(run.start["time"]).isoformat()
+    scaninfo["command"] = get_with_fallbacks(
+        run.start, "command", "plan_name", default=None
+    )
+    scaninfo["element"] = get_with_fallbacks(run.start, "element", "edge", default=None)
+    scaninfo["motor"] = convert_names(run.start["motors"][0])
+    scankeys = [
+        "time",
+        "users",
+        "proposal",
+        "cycle",
+        "saf",
+        "group_md",
+        "beamtime_uid",
+        "beamtime_start",
+        "repeat",
+    ]
     for k in scankeys:
         if k in run.start:
             scaninfo[k] = run.start[k]
-    if 'ref_args' in run.start:
-        scaninfo['ref_edge'] = run.start['ref_args']['i0up_multimesh_sample_sample_name']['value']
-        scaninfo['ref_id'] = run.start['ref_args']['i0up_multimesh_sample_sample_id']['value']
-    scaninfo['raw_uid'] = run.start['uid']
+    if "ref_args" in run.start:
+        scaninfo["ref_edge"] = run.start["ref_args"][
+            "i0up_multimesh_sample_sample_name"
+        ]["value"]
+        scaninfo["ref_id"] = run.start["ref_args"]["i0up_multimesh_sample_sample_id"][
+            "value"
+        ]
+    scaninfo["raw_uid"] = run.start["uid"]
     motors = {}
     baseline = run.baseline.data.read()
-    motors['exslit'] = get_with_fallbacks(baseline, 'Exit Slit of Mono Vertical Gap')[0].item()
-    motors['manipx'] = get_with_fallbacks(baseline, 'manip_x', 'Manipulator_x')[0].item()
-    motors['manipy'] = get_with_fallbacks(baseline, 'manip_y', 'Manipulator_y')[0].item()
-    motors['manipz'] = get_with_fallbacks(baseline, 'manip_z', 'Manipulator_z')[0].item()
-    motors['manipr'] = get_with_fallbacks(baseline, 'manip_r', 'Manipulator_r')[0].item()
-    motors['samplex'] = get_with_fallbacks(baseline, 'manip_sx', 'Manipulator_sx')[0].item()
-    motors['sampley'] = get_with_fallbacks(baseline, 'manip_sy', 'Manipulator_sy')[0].item()
-    motors['samplez'] = get_with_fallbacks(baseline, 'manip_sz', 'Manipulator_sz')[0].item()
-    motors['sampler'] = get_with_fallbacks(baseline, 'manip_sr', 'Manipulator_sr')[0].item()
-    motors['tesz'] = get_with_fallbacks(baseline, 'tesz')[0].item()
-    metadata['scaninfo'] = scaninfo
-    metadata['motors'] = motors
-    metadata['channelinfo'] = {}
+    motors["exslit"] = get_with_fallbacks(baseline, "Exit Slit of Mono Vertical Gap")[
+        0
+    ].item()
+    motors["manipx"] = get_with_fallbacks(baseline, "manip_x", "Manipulator_x")[
+        0
+    ].item()
+    motors["manipy"] = get_with_fallbacks(baseline, "manip_y", "Manipulator_y")[
+        0
+    ].item()
+    motors["manipz"] = get_with_fallbacks(baseline, "manip_z", "Manipulator_z")[
+        0
+    ].item()
+    motors["manipr"] = get_with_fallbacks(baseline, "manip_r", "Manipulator_r")[
+        0
+    ].item()
+    motors["samplex"] = get_with_fallbacks(baseline, "manip_sx", "Manipulator_sx")[
+        0
+    ].item()
+    motors["sampley"] = get_with_fallbacks(baseline, "manip_sy", "Manipulator_sy")[
+        0
+    ].item()
+    motors["samplez"] = get_with_fallbacks(baseline, "manip_sz", "Manipulator_sz")[
+        0
+    ].item()
+    motors["sampler"] = get_with_fallbacks(baseline, "manip_sr", "Manipulator_sr")[
+        0
+    ].item()
+    motors["tesz"] = get_with_fallbacks(baseline, "tesz")[0].item()
+    metadata["scaninfo"] = scaninfo
+    metadata["motors"] = motors
+    metadata["channelinfo"] = {}
     return metadata
 
 
 def get_run_data(run, exportArrayData=False):
 
-    natural_order = ["Seconds", "MONO", "ENERGY_ENC", "I0", "I1", "REF", "SC",
-                     "tfy"]
-    config = run.primary.descriptors[0]['configuration']
-    exposure = get_with_fallbacks(config, ['ucal_i0up', 'data', 'ucal_i0up_exposure_time'],
-                                  ['ucal_sc', 'data', 'ucal_sc_exposure_time'],
-                                  ['ucal_i400_i0up', 'data', 'ucal_i400_i0up_exposure_time'],
-                                  ['ucal_i400_sc', 'data', 'ucal_i400_sc_exposure_time'],
-                                  ['ucal_I400', 'data', 'ucal_I400_exposure_sp'])
+    natural_order = ["Seconds", "MONO", "ENERGY_ENC", "I0", "I1", "REF", "SC", "tfy"]
+    config = run.primary.descriptors[0]["configuration"]
+    exposure = get_with_fallbacks(
+        config,
+        ["nexafs_i0up", "data", "nexafs_i0up_exposure_time"],
+        ["ucal_i0up", "data", "ucal_i0up_exposure_time"],
+        ["ucal_sc", "data", "ucal_sc_exposure_time"],
+        ["ucal_i400_i0up", "data", "ucal_i400_i0up_exposure_time"],
+        ["ucal_i400_sc", "data", "ucal_i400_sc_exposure_time"],
+        ["ucal_I400", "data", "ucal_I400_exposure_sp"],
+    )
     exposure = float(exposure)
     columns = []
     datadict = {}
-    known_array_keys = ['tes_mca_spectrum']
-    statistics_re = re.compile('_median$|_std$|_npts$|_sum$')
+    known_array_keys = ["tes_mca_spectrum"]
+    statistics_re = re.compile("_median$|_std$|_npts$|_sum$")
     keys = run.primary.data.keys()
     usekeys = []
 
@@ -105,8 +157,8 @@ def get_run_data(run, exportArrayData=False):
         newkey = convert_names(key)
         if len(data[key].shape) == 1 or exportArrayData:
             datadict[newkey] = data[key].data
-    if 'Seconds' not in datadict:
-        datadict['Seconds'] = np.zeros_like(datadict[newkey]) + exposure
+    if "Seconds" not in datadict:
+        datadict["Seconds"] = np.zeros_like(datadict[newkey]) + exposure
     for k in natural_order:
         if k in datadict.keys():
             columns.append(k)
@@ -140,8 +192,8 @@ def get_data_and_header(run, infer_rois=True, rois=[], channels=None):
     columns, run_data = get_run_data(run)
     _rois = {}
     if infer_rois:
-        _rois['tfy'] = roiMaster['tfy']
-        e = header['scaninfo'].get('element', '').lower()
+        _rois["tfy"] = roiMaster["tfy"]
+        e = header["scaninfo"].get("element", "").lower()
         if e in roiDefaults:
             for k in roiDefaults[e]:
                 _rois[k] = roiMaster[k]
@@ -159,36 +211,38 @@ def get_data_and_header(run, infer_rois=True, rois=[], channels=None):
             columns.append(k)
             run_data.append(tes_data[i])
     data = np.vstack(run_data).T
-    header['channelinfo']['cols'] = columns
-    header['channelinfo']['weights'] = {}
-    header['channelinfo']['offsets'] = {}
-    header['channelinfo']['rois'] = {}
+    header["channelinfo"]["cols"] = columns
+    header["channelinfo"]["weights"] = {}
+    header["channelinfo"]["offsets"] = {}
+    header["channelinfo"]["rois"] = {}
     for key, roi in _rois.items():
-        header['channelinfo']['rois'][key] = roi
+        header["channelinfo"]["rois"][key] = roi
     return data, header
 
 
-def export_run_to_analysis_catalog(run, infer_rois=True, rois=[], channels=None, check_existing=True):
+def export_run_to_analysis_catalog(
+    run, infer_rois=True, rois=[], channels=None, check_existing=True
+):
     global ANALYSIS_CATALOG
     if ANALYSIS_CATALOG is None:
         from tiled.client import from_profile
-        c = from_profile('nsls2')['ucal']['sandbox']
+
+        c = from_profile("nsls2")["ucal"]["sandbox"]
         ANALYSIS_CATALOG = c
     if check_existing:
-        uid = run.metadata['start']['uid']
+        uid = run.metadata["start"]["uid"]
         if len(ANALYSIS_CATALOG.search(Key("scaninfo.raw_uid") == uid)) > 0:
             print("Data associated with this run is already in the catalog")
             return
 
-    data, header = get_data_and_header(run, infer_rois=infer_rois, rois=rois,
-                                       channels=channels)
+    data, header = get_data_and_header(
+        run, infer_rois=infer_rois, rois=rois, channels=channels
+    )
 
     try:
-        new_uid = ANALYSIS_CATALOG.write_array(data, metadata=header,
-                                               specs=['nistxas'])
+        new_uid = ANALYSIS_CATALOG.write_array(data, metadata=header, specs=["nistxas"])
     except HTTPStatusError:
         print("Got an HTTP Error, will sleep and retry once")
         sleep(1)
-        new_uid = ANALYSIS_CATALOG.write_array(data, metadata=header,
-                                               specs=['nistxas'])
+        new_uid = ANALYSIS_CATALOG.write_array(data, metadata=header, specs=["nistxas"])
     return new_uid
--- ucalpost/databroker/run.py
+++ ucalpost/databroker/run.py
@@ -1,19 +1,19 @@
-#from tiled.client import from_profile
+# from tiled.client import from_profile
 from os import path
 from databroker.queries import TimeRange
 import datetime
 import numpy as np
 
 
-#db = from_profile("ucal")
+# db = from_profile("ucal")
 
 
 def get_config_dict(run):
-    return run.primary.descriptors[0]['configuration']['tes']['data']
+    return run.primary.descriptors[0]["configuration"]["tes"]["data"]
 
 
 def get_filename(run, convert_local=True):
-    filename = get_config_dict(run)['tes_filename']
+    filename = get_config_dict(run)["tes_filename"]
     if convert_local:
         raw_directory = get_raw_directory(filename)
         a, name = path.split(filename)
@@ -24,8 +24,8 @@ def get_filename(run, convert_local=True):
 
 def get_logname(run):
     config = get_config_dict(run)
-    state_str = 'calibration' if config['tes_cal_flag'] else 'scan'
-    scan_num = config['tes_scan_num']
+    state_str = "calibration" if config["tes_cal_flag"] else "scan"
+    scan_num = config["tes_scan_num"]
     filebase = path.dirname(get_filename(run))
     filename = f"{state_str}{scan_num:0=4d}.json"
     return path.join(filebase, "logs", filename)
@@ -33,13 +33,13 @@ def get_logname(run):
 
 def get_save_directory(run):
     basename = "/nsls2/data/sst/legacy/ucal/processed"
-    timestamp = run.metadata['start']['time']
+    timestamp = run.metadata["start"]["time"]
     date = datetime.datetime.fromtimestamp(timestamp)
     return path.join(basename, f"{date.year}/{date.month:02d}/{date.day:02d}")
 
 
 def get_raw_directory(filename):
-    date = path.basename(filename).split('_')[0]
+    date = path.basename(filename).split("_")[0]
     year = date[:4]
     month = date[4:6]
     day = date[6:]
@@ -48,9 +48,11 @@ def get_raw_directory(filename):
 
 
 def get_proposal_directory(run):
-    passid = run.metadata['start']['proposal']
-    date = datetime.datetime.fromtimestamp(run.metadata['start'].get('beamtime_start', run.metadata['start']['time']))
-    cycle = run.metadata['start']['cycle']
+    passid = run.metadata["start"]["proposal"]
+    date = datetime.datetime.fromtimestamp(
+        run.metadata["start"].get("beamtime_start", run.metadata["start"]["time"])
+    )
+    cycle = run.metadata["start"]["cycle"]
     dirname = f"/nsls2/data/sst/legacy/ucal/proposals/{date.year}-{cycle}/pass-{passid}/ucal/{date.year}{date.month:02}{date.day:02}"
     return dirname
 
@@ -58,40 +60,42 @@ def get_proposal_directory(run):
 def get_tes_state(run):
     config = get_config_dict(run)
     if "tes_scan_str" in config:
-        state = config['tes_scan_str']
+        state = config["tes_scan_str"]
     else:
-        state_str = 'CAL' if config['tes_cal_flag'] else 'SCAN'
-        scan_num = config['tes_scan_num']
+        state_str = "CAL" if config["tes_cal_flag"] else "SCAN"
+        scan_num = config["tes_scan_num"]
         state = f"{state_str}{scan_num}"
     return state
 
 
 def get_line_names(cal_run):
-    if 'cal_lines' in cal_run.start:
-        return cal_run.start['cal_lines']
+    if "cal_lines" in cal_run.start:
+        return cal_run.start["cal_lines"]
     samplename = get_samplename(cal_run)
-    if samplename == 'mixv1':
-        energy = cal_run.start.get('calibration_energy', 980)
+    if samplename == "mixv1":
+        energy = cal_run.start.get("calibration_energy", 980)
         line_energies = np.array([300, 400, 525, 715, 840, 930, 950, 1020])
-        line_names = np.array(['ck', 'nk', 'ok', 'fela', 'nila', 'cula', "culb", 'znla'])
+        line_names = np.array(
+            ["ck", "nk", "ok", "fela", "nila", "cula", "culb", "znla"]
+        )
         return list(line_names[line_energies < energy])
     else:
         return ["ck", "nk", "ok", "fela", "nila", "cula"]
 
 
 def get_cal_id(run, default=None):
-    cal_id = run.start['last_cal']
+    cal_id = run.start["last_cal"]
     if cal_id is None:
         return default
     return cal_id
 
 
 def get_cal(run, catalog):
-    return catalog[run.start['last_cal']]
+    return catalog[run.start["last_cal"]]
 
 
 def getRunFromStop(doc, catalog):
-    run_uuid = doc['run_start']
+    run_uuid = doc["run_start"]
     run = catalog[run_uuid]
     return run
 
@@ -99,33 +103,45 @@ def getRunFromStop(doc, catalog):
 def get_samplename(run):
     if "sample_name" in run.start:
         return run.start["sample_name"]
-    elif 'sample_md' in run.start:
-        return run.start['sample_md']['name']
-    elif 'sample_args' in run.start:
-        return run.start['sample_args']['sample_name']['value']
+    elif "sample_md" in run.start:
+        return run.start["sample_md"]["name"]
+    elif "sample_args" in run.start:
+        return run.start["sample_args"]["sample_name"]["value"]
+    else:
+        return "None"
+
+
+def get_sampleid(run):
+    if "sample_id" in run.start:
+        return run.start["sample_id"]
+    elif "sample_md" in run.start:
+        return run.start["sample_md"]["sample_id"]
+    elif "sample_args" in run.start:
+        return run.start["sample_args"]["sample_id"]["value"]
     else:
         return "None"
 
 
 def summarize_run(run):
-    scanid = run.metadata['start']['scan_id']
+    scanid = run.metadata["start"]["scan_id"]
     sample = get_samplename(run)
-    scantype = run.start.get('scantype', 'None')
+    scantype = run.start.get("scantype", "None")
 
     print(f"Scan {scanid}")
-    if 'group_md' in run.start:
+    if "group_md" in run.start:
         print(f"Group: {run.start['group_md']['name']}")
-    elif 'group' in run.start:
+    elif "group" in run.start:
         print(f"Group: {run.start['group']}")
     print(f"Sample name: {sample}")
-    if scantype == 'xas':
-        edge = run.start.get('edge', 'Not recorded')
+    if scantype == "xas":
+        edge = run.start.get("edge", "Not recorded")
         print(f"Scantype: {scantype}, edge: {edge}")
     else:
         print(f"Scantype: {scantype}")
-    if 'last_cal' in run.start:
+    if "last_cal" in run.start:
         print(f"Calibration: {run.start['last_cal']!s:.8}")
 
-#tes_runs = db.search(TimeRange(since="2022-01-26", until="2022-01-28"))
-#sample_runs = tes_runs.search({"sample_args": {"$exists": True}})
-#run = sample_runs[-1]
+
+# tes_runs = db.search(TimeRange(since="2022-01-26", until="2022-01-28"))
+# sample_runs = tes_runs.search({"sample_args": {"$exists": True}})
+# run = sample_runs[-1]
